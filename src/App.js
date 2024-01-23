94730a43a31aa791de7332bcc02950a5cd72a555
Ewan Stott <ewan.stott1@gmail.com>
2024-01-23 21:40:34 +0100

    commit with header, main and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import { Header } from "./components/Header.jsx";
 import { Footer } from "./components/Footer.jsx";
-import { Links } from "./components/Links.jsx";
 import { Main } from "./components/Main.jsx";
 
 class App extends Component {
-  render() {}
+  render() {
+    return (
+      <div>
+        <Header />
+        <aside></aside>
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
