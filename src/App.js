515c4b69ee2f3780712b528fb47336e350dae8ba
Ewan Stott <ewan.stott1@gmail.com>
2024-01-24 07:42:50 +0100

    re-added simple react counter app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
+// EXAMPLE 1 - Hello World
+// //imrc -> import React
+// import React, { Component } from "react";
+
+// //cc -> to write a class -> this section shows in the DOM
+// class App extends Component {
+//   state = {};
+//   render() {
+//     return <p>HELLO WORLD</p>;
+//   }
+// }
+
+// //cc -> export class
+// export default App;
+
+//EXAMPLE 2 - REACT COUNTER
 import React, { Component } from "react";
-import { Header } from "./components/Header.jsx";
-import { Footer } from "./components/Footer.jsx";
-import { Main } from "./components/Main.jsx";
 
 class App extends Component {
+  //state holds data
+  state = { count: 1 };
+
+  //declare functions - e.g. using setState function
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  //this.state.count connects to state
   render() {
     return (
       <div>
-        <Header />
-        <aside></aside>
-        <Main />
-        <Footer />
+        <p>{this.state.count}</p>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
       </div>
     );
   }
